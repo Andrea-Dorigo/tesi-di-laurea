b3cf5dc847dab4ba8f405550556534b9d0d34fdf
Andrea-Dorigo <failosc@gmail.com>
2021-05-16 19:19:54 +0200

    Make script easier to read

--- compilepdf.sh
+++ compilepdf.sh
@@ -1,1 +1,4 @@
-pdflatex -output-directory=aux tesi.tex && biber --output_directory aux tesi.bcf && pdflatex -output-directory=aux tesi.tex && mv aux/tesi.pdf . 
+pdflatex -output-directory=aux tesi.tex
+biber --output_directory aux tesi.bcf
+pdflatex -output-directory=aux tesi.tex
+mv aux/tesi.pdf .
