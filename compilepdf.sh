ce9970422004b3edbb3c99d8f815c9acf7186371
Andrea-Dorigo <failosc@gmail.com>
2021-09-10 14:46:16 +0200

    Complete chapter 1-2 rework - approved by Tullio

--- compilepdf.sh
+++ compilepdf.sh
@@ -1,5 +1,5 @@
-pdflatex -output-directory=aux tesi.tex
+pdflatex --halt-on-error -shell-escape -output-directory=aux tesi.tex
 makeglossaries -d aux tesi
 biber --output_directory aux tesi.bcf
-pdflatex -output-directory=aux tesi.tex
+pdflatex --halt-on-error -shell-escape -output-directory=aux tesi.tex
 mv aux/tesi.pdf .
